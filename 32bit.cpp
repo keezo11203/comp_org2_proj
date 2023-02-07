89d4ca181cce14185216acc7ded75ed00130cc05
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2023-02-07 15:10:12 -0500

    Create 32bit.cpp
    start timer

---
+++ 32bit.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <chrono>
+
+int main() 
+{
+    // Start timer
+    std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
