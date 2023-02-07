7259d537f695460c0854f24a5dbe471b0b0c9611
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2023-02-07 15:41:44 -0500

    Update 32bit.cpp
    10^10 additions

--- 32bit.cpp
+++ 32bit.cpp
@@ -3,5 +3,14 @@
 
 int main() 
 {
-    // Start timer
+ //Start timer
     std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+    
+ // Initialize a variable to store the result
+    int result = 0;
+ // Loop through the range from 0 to 10^10
+    for (int i = 0; i < 10000000000; i++) 
+{
+ // Add the current value of i to the result
+    result += i;
+}
