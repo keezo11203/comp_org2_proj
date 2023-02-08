d33a54d0006ee747b67e933603e3f99b8e14ef68
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2023-02-07 21:08:54 -0500

    write into file

--- HDBench2.cpp
+++ HDBench2.cpp
@@ -6,3 +6,11 @@ int main()
 {
     // Start timer
     std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+    
+    // Hard drive operations
+    std::ofstream f("file.txt", std::ios::binary);
+    for (int i = 0; i < 1000000000 / 10000; i++) 
+    {
+        f.write("0", 10000);
+    }
+    f.close();
