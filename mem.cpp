17a9b0b2d40b5d8d5f212235d5e7535853d2a25a
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2023-02-07 20:38:45 -0500

    create array

--- mem.cpp
+++ mem.cpp
@@ -4,3 +4,6 @@
 int main() {
     // record the starting time
     std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+    
+    // allocate an array of ints
+    int *arr = new int[5 * 10000000];
