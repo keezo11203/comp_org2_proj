383e2790171d9abd52728fd4a188d366fd96ad89
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2023-02-07 20:37:30 -0500

    create .cpp for memory benchmark and record starting time

---
+++ mem.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <chrono>
+
+int main() {
+    // record the starting time
+    std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
