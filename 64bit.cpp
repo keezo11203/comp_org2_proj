cff897f1ff79ee379658d433d4fabc9fc84016e2
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2023-02-07 20:29:35 -0500

    Update 64bit.cpp
    10^10 additions

--- 64bit.cpp
+++ 64bit.cpp
@@ -6,3 +6,10 @@ int main()
 {
 // Start timer
 std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+  
+  // Float operations
+double result = 0.0;
+for (int i = 0; i < 1000000000; i++) 
+{
+    result += i;
+}
