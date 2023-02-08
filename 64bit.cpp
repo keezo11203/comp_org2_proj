3d07e6c42d75bfadfb62686284e137579dbb2dac
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2023-02-07 20:28:28 -0500

    Update 64bit.cpp
    include statements and timer

--- 64bit.cpp
+++ 64bit.cpp
@@ -1,1 +1,8 @@
+#include <iostream>
+#include <chrono>
+#include <cmath>
 
+int main()
+{
+// Start timer
+std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
