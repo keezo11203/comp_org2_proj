7734395b1caeec97285fb5543da6f98d493a5644
Kaitlyn <91761833+keezo11203@users.noreply.github.com>
2023-02-07 21:02:23 -0500

    include statements and starting timer

---
+++ HDBench2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <chrono>
+#include <fstream>
+
+int main()
+{
+    // Start timer
+    std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
